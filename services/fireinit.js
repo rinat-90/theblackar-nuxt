658bc49d61cf7ebe600d4138b193fdb22d320897
Rinat Ibragimov <ribroff@gmail.com>
2020-04-14 12:20:09 -0500

    Update fireinit.js

--- services/fireinit.js
+++ services/fireinit.js
@@ -3,15 +3,16 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/database'
 
-let config = {
-  apiKey: "xxxxx",
-  authDomain: "xxxxx",
-  databaseURL: "xxxx",
-  projectId: "xxx9",
-  storageBucket: "xxxx",
-  messagingSenderId: "xxxx",
-  appId: "xxxxx"
-};
+const config = {
+  apiKey: "AIzaSyAarXSwTBc_HhdYliM5932B1L3QLgbEKp0",
+  authDomain: "vuetify-cfb29.firebaseapp.com",
+  databaseURL: "https://vuetify-cfb29.firebaseio.com",
+  projectId: "vuetify-cfb29",
+  storageBucket: "vuetify-cfb29.appspot.com",
+  messagingSenderId: "230845894670",
+  appId: "1:230845894670:web:9b9b403e540db560"
+
+}
 
 
 !firebase.apps.length ? firebase.initializeApp(config) : '';
